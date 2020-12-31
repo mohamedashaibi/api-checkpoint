28befcfe73998e6f0c45d8d4cef958f422bd92cf
yomj1 <mohashaibi1@gmail.com>
2020-12-31 17:15:56 +0200

    first commin to api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [users,setUsers] = useState([])
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users').then(res=>setUsers(res.data))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user)=><div className="user">
+        <h1>{user.id}</h1>
+        <h1>{user.name}</h1>
+        <h1>{user.username}</h1>
+      </div>)}
     </div>
   );
 }
